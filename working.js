8701d4d46db7c3f583734da65e8fcdfc17c4c787
Ranjith4701 <ranjithjagan786@gmail.com>
2023-09-26 17:48:27 +0530

    added

---
+++ working.js
@@ -0,0 +1,20 @@
+let fruits = ["banana", "apple", "mango", "grapes", "kiwi"];
+console.log(fruits);
+console.log(fruits.length);
+fruits.push("orrange");
+console.log(fruits);
+fruits.unshift("jackfruit");
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits.slice(2);
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+
+
+
+
